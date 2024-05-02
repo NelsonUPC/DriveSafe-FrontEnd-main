95346c2d8b3d0e69bb8a95d0a288cc0e6da0dab8
Leonel-Alfaro <u20201a930@upc.edu.pe>
2024-05-02 17:56:26 -0500

    fix: update

--- src/DriveSafe/services/user.service.js
+++ src/DriveSafe/services/user.service.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import authHeader from './auth-header';
 import http from "@/shared/services/http-common";
 
-const API_URL = 'https://autoya.azurewebsites.net/api/v1/';
+const API_URL = 'https://DriveSafeprueva.azurewebsites.net/api/v1/';
 
 class UserService {
 
