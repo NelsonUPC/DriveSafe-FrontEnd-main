4fc1c3dddac7d02ace975bb3e360cefbaf08cc84
NelsonUPC <164695009+NelsonUPC@users.noreply.github.com>
2024-05-03 05:34:59 -0500

    fix: resolve issue with profile tenant data

--- src/DriveSafe/pages/profile-tenant.vue
+++ src/DriveSafe/pages/profile-tenant.vue
@@ -1,7 +1,7 @@
 <script>
 import ArrendatarioService from "@/DriveSafe/services/arrendatario.service";
 export default {
-  data(){
+  data() {
     return {
       user: {
         name: '',
@@ -12,30 +12,24 @@ export default {
       },
       drawer: false,
       items: [
-        { label: "Inicio", to: "/home" },
-        { label: "Buscar Autos", to: "/car-search-tenant" },
-        { label: "Mantenimiento", to: "/manteinance-tenant" },
-        { label: "Alquiler", to: "/rent-tenant" },
+        {label: "Inicio", to: "/home"},
+        {label: "Buscar Autos", to: "/car-search-tenant"},
+        {label: "Mantenimiento", to: "/manteinance-tenant"},
+        {label: "Alquiler", to: "/rent-tenant"},
       ],
     };
   },
   methods: {
     async cargarInformacionArrendatario() {
       try {
-        const response = await ArrendatarioService.getAll();
+        this.user.name = localStorage.getItem("usuarioNombres");
 
-        const arrendatario = response.data.find(
-            (arrendatario) =>
-                arrendatario.id === parseInt(localStorage.getItem("arrendatarioId"))
-        );
+        this.user.lastName = localStorage.getItem("usuarioApellidos");
 
-        if (arrendatario) {
-          this.user.name = arrendatario.nombres;
-          this.user.lastName = arrendatario.apellidos;
-          this.user.phone = arrendatario.telefono;
-          this.user.email = arrendatario.correo;
-          this.user.birthday = arrendatario.fechaNacimiento;
-        }
+        this.user.phone = localStorage.getItem("usuarioCelular");
+
+        this.user.email = localStorage.getItem("usuarioCorreo");
+
       } catch (error) {
         console.error("Error al cargar la información del propietario:", error);
       }
@@ -43,24 +37,21 @@ export default {
     cerrarSesion() {
 
       this.$router.push('/login');
-
-      localStorage.setItem("arrendatarioId", null);
-      localStorage.setItem("fotoTenant", "https://cdn.icon-icons.com/icons2/1378/PNG/512/avatardefault_92824.png")
+      localStorage.clear()
     },
   },
   created() {
-
     this.cargarInformacionArrendatario();
   },
 };
 </script>
 
 <template>
-  <pv-toast aria-live = "polite" />
+  <pv-toast/>
   <header aria-label="Barra de navegación">
     <pv-toolbar class="custom-bg custom-toolbar">
       <template #start>
-        <img src="https://i.imgur.com/hIAgH3Z.png" alt="Logo" style="height: 40px; margin-right: 20px;"/>
+        <img src="https://imgur.com/a/DWk9R7P" alt="Logo" style="height: 40px; margin-right: 20px;"/>
       </template>
       <template #end>
         <div class="flex-column">
@@ -81,7 +72,9 @@ export default {
             </pv-button>
           </router-link>
           <router-link to="/profile-tenant" aria-label="Enlace al perfil del arrendatario">
-            <img src="https://cdn.icon-icons.com/icons2/1378/PNG/512/avatardefault_92824.png" alt="Usuario" style="height: 30px; margin-left: 20px; cursor: pointer;" aria-label="Imagen de perfil del arrendatario"/>
+            <img src="https://cdn.icon-icons.com/icons2/1378/PNG/512/avatardefault_92824.png" alt="Usuario"
+                 style="height: 30px; margin-left: 20px; cursor: pointer;"
+                 aria-label="Imagen de perfil del arrendatario"/>
           </router-link>
         </div>
       </template>
@@ -98,24 +91,27 @@ export default {
         <h2>Nombres: </h2>
         <h2>{{ user.name }}</h2><br>
         <h2>Apellidos: </h2>
-        <h2>{{user.lastName}}</h2><br>
+        <h2>{{ user.lastName }}</h2><br>
         <h2>Celular: </h2>
-        <h2>{{user.phone}}</h2><br>
+        <h2>{{ user.phone }}</h2><br>
         <h2>Correo: </h2>
-        <h2>{{user.email}}</h2><br>
+        <h2>{{ user.email }}</h2><br>
       </div>
       <div class="buttons" aria-label="Botones del perfil del usuario">
         <router-link to="/update-tenant" aria-label="Enlace para actualizar datos del arrendatario">
-          <pv-button class="font-button">Actualizar Datos</pv-button><br>
+          <pv-button class="font-button">Actualizar Datos</pv-button>
+          <br>
         </router-link>
 
-        <pv-button class="font-button" @click="cerrarSesion" aria-label="Botón para cerrar sesión">Cerrar Sesión</pv-button>
+        <pv-button class="font-button" @click="cerrarSesion" aria-label="Botón para cerrar sesión">Cerrar Sesión
+        </pv-button>
       </div>
     </div>
     <div class="right-column" aria-label="Columna derecha del perfil del usuario">
       <div class="profile-image-container" aria-label="Contenedor de la imagen de perfil del usuario">
         <div class="profile-image">
-          <img src="https://cdn.icon-icons.com/icons2/1378/PNG/512/avatardefault_92824.png" alt="Profile Picture" class="size-photo" aria-label="Imagen de perfil del usuario"/>
+          <img src="https://cdn.icon-icons.com/icons2/1378/PNG/512/avatardefault_92824.png" alt="Profile Picture"
+               class="size-photo" aria-label="Imagen de perfil del usuario"/>
         </div>
         <br>
       </div>
@@ -126,26 +122,30 @@ export default {
 
 
 <style scoped>
-body{
+body {
   font-family: 'Poppins', sans-serif;
-  color: #000000; 
+  color: #000000;
   background-color: white;
 }
+
 .custom-bg {
   background-color: white;
 }
+
 .custom-button, .font-button {
   background-color: #ffffff;
 }
+
 .custom-button:hover,
 .custom-button:focus {
-  background-color: #1A2C63 !important; 
+  background-color: #1A2C63 !important;
   color: white !important;
 }
 
 .custom-toolbar {
   border-bottom: 2px solid #ddd;
 }
+
 .profile-button {
   text-align: center;
   margin-top: 10px;
@@ -156,13 +156,14 @@ body{
   flex-direction: column;
   align-items: flex-start;
 }
+
 .font-button {
   margin: 2px 0;
   background-color: #1A2C63;
   color: white;
 }
 
-.font-button:hover{
+.font-button:hover {
   background-color: #FFA500;
   color: white;
 }
@@ -189,16 +190,16 @@ body{
   align-items: center;
 }
 
-.title h1{
+.title h1 {
   margin-top: 0;
 }
 
-.size-photo{
+.size-photo {
   max-width: 50%;
   max-height: 50%;
 }
 
-.profile-image-container{
+.profile-image-container {
   background-color: #f5f5f5;
   border: 2px solid #ccc;
   padding: 20px;
@@ -207,17 +208,17 @@ body{
   height: 50%;
 }
 
-.profile-info{
+.profile-info {
   margin-top: 40px;
   margin-bottom: 20px;
 }
 
-.profile-info p{
+.profile-info p {
   margin: 10px;
   line-height: 2.5;
 }
 
-.profile-image{
+.profile-image {
   display: flex;
   align-items: center;
   justify-content: center;
