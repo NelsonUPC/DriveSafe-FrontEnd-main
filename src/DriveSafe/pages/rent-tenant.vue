cc4799d5e9b196a60638a8e0cb8c39540f0aaa9a
NelsonUPC <164695009+NelsonUPC@users.noreply.github.com>
2024-05-03 05:40:14 -0500

    fix: resolve rent tenant data

--- src/DriveSafe/pages/rent-tenant.vue
+++ src/DriveSafe/pages/rent-tenant.vue
@@ -1,6 +1,9 @@
 <script>
 import Card from "primevue/card";
 import VehiculoService from "@/DriveSafe/services/vehiculo.service";
+import AlquilerService from "@/DriveSafe/services/alquiler.service";
+import UserService from "@/DriveSafe/services/user.service";
+import router from "@/router";
 
 export default {
   components: {
@@ -10,130 +13,145 @@ export default {
     return {
       drawer: false,
       items: [
-        { label: "Inicio", to: "/home" },
-        { label: "Buscar Autos", to: "/car-search-tenant" },
-        { label: "Mantenimiento", to: "/manteinance-tenant" },
-        { label: "Alquiler", to: "/rent-tenant" },
+        {label: "Inicio", to: "/home"},
+        {label: "Buscar Autos", to: "/car-search-tenant"},
+        {label: "Mantenimiento", to: "/manteinance-tenant"},
+        {label: "Alquiler", to: "/rent-tenant"},
       ],
-      vehiculos: [],
-      vehiculosFiltrados: [],
+      alquileres: [],
     };
   },
   methods: {
-    async cargarVehiculos() {
+    async cargarAlquileres() {
       try {
-        const response = await VehiculoService.getAll();
-        this.vehiculos = response.data;
-        this.vehiculosFiltrados = this.vehiculos.filter(vehiculo => vehiculo.arrendatario && vehiculo.arrendatario.id === parseInt(localStorage.getItem("arrendatarioId")));
+        const usuarioId = parseInt(localStorage.getItem("usuarioId"));
+        console.log("Usuario ID:", usuarioId);
+        const response = await AlquilerService.getByUserId(usuarioId);
+        console.log("Response:", response);
+        this.alquileres = response.data;
+
+        // Obtener detalles del vehículo y del propietario para cada alquiler
+        for (const alquiler of this.alquileres) {
+          const vehiculoResponse = await VehiculoService.getById(alquiler.vehiculo_id);
+          console.log("Vehículo Response:", vehiculoResponse);
+          alquiler.vehiculo = vehiculoResponse.data;
+
+          const propietarioResponse = await UserService.getUserById(alquiler.propietario_id);
+          console.log("Propietario Response:", propietarioResponse);
+          alquiler.propietario = propietarioResponse.data;
+        }
+
+        console.log("Alquileres:", this.alquileres);
       } catch (error) {
-        console.error("Error al cargar los vehículos:", error);
+        console.error("Error al cargar los alquileres:", error);
       }
     },
+    irAPagar(alquiler) {
+      localStorage.setItem("alquilerId", alquiler.id)
+      localStorage.setItem("vehiculoId", alquiler.vehiculo.id)
+      localStorage.setItem("vehiculoPrecio", alquiler.vehiculo.costo_alquiler)
+      localStorage.setItem("vehiculoTiempo", alquiler.vehiculo.tipo_tiempo)
+      localStorage.setItem("vehiculoFi", alquiler.fecha_inicio)
+      localStorage.setItem("vehiculoFf", alquiler.fecha_fin)
+      localStorage.setItem("vehiculoLugar", alquiler.vehiculo.lugar_recojo)
+      localStorage.setItem("vehiculoFoto", alquiler.vehiculo.url_imagen)
+      console.log("Ir a pagar por el alquiler:", alquiler);
+
+
+      router.push({path: "/rent-payment"});
+    }
   },
   created() {
-    this.cargarVehiculos();
-  }
+    this.cargarAlquileres();
+  },
 };
 </script>
 
 <template>
-  <pv-toast aria-live="polite" />
-  <header role="banner">
-    <pv-toolbar class="custom-bg custom-toolbar" role="navigation">
-      <template #start>
-        <img
-            src="https://i.imgur.com/hIAgH3Z.png"
-            alt="Logo"
-            style="height: 40px; margin-right: 20px;"
-        />
-      </template>
-      <template #end>
-        <div class="flex-column" role="menubar">
-          <router-link
-              v-for="item in items"
-              :to="item.to"
-              custom
-              v-slot="{ navigate, href }"
-              :key="item.label"
-              role="menuitem"
-          >
-            <pv-button
-                class="custom-button"
-                :href="href"
-                @click="navigate"
-                role="button"
+  <div>
+    <header>
+      <pv-toolbar class="custom-bg custom-toolbar">
+        <template #start>
+          <img src="https://i.postimg.cc/2jd7PRtj/Drive-Safe-Logo.png"
+               alt="Logo"
+               style="height: 70px; margin-right: 20px;"
+               aria-label="DriveSafe Logo"/>
+        </template>
+        <template #end>
+          <div class="flex-column">
+            <router-link
+                v-for="item in items"
+                :to="item.to"
+                custom
+                v-slot="{ navigate, href }"
+                :key="item.label"
             >
-              {{ item.label }}
-            </pv-button>
-          </router-link>
-          <router-link to="/profile-tenant" role="menuitem">
-            <img
-                src="https://i.postimg.cc/Fs9Z3g3V/usuario-1.png"
-                alt="Usuario"
-                style="height: 30px; margin-left: 20px; cursor: pointer;"
-            />
-          </router-link>
-        </div>
-      </template>
-    </pv-toolbar>
-  </header>
+              <pv-button class="custom-button" :href="href" @click="navigate">{{ item.label }}</pv-button>
+            </router-link>
+            <router-link to="/profile-tenant">
+              <img src="https://i.postimg.cc/Fs9Z3g3V/usuario-1.png" alt="Usuario"
+                   style="height: 30px; margin-left: 20px; cursor: pointer;"/>
+            </router-link>
+          </div>
+        </template>
+      </pv-toolbar>
+    </header>
 
-  <div class="p-grid" role="main">
-    <h1 id="vehiclesTitle" style="font-family: 'Poppins', sans-serif; color: #FF7A00">Vehículos alquilados</h1>
-    <div class="card-container" role="region" aria-labelledby="vehiclesTitle">
-      <div v-if="vehiculosFiltrados.length > 0" class="card-container">
-        <div class="card-item" v-for="vehiculo in vehiculosFiltrados" :key="vehiculo.id">
-          <Card role="region" aria-labelledby="cardTitle{{vehiculo.id}}">
-            <template #title></template>
+    <div class="p-grid">
+      <h1 style="font-family: 'Poppins', sans-serif; color: #FF7A00">Alquileres solicitados por el usuario</h1>
+      <div v-if="alquileres.length === 0">
+        <p>No se encontraron alquileres solicitados.</p>
+      </div>
+      <div v-else>
+        <div v-for="alquiler in alquileres" :key="alquiler.id">
+          <Card class="custom-card">
+            <template #title>
+              <h2>Detalles del alquiler {{ alquiler.id }}</h2>
+            </template>
             <template #content>
-              <img :src="vehiculo.urlImagen" alt="Imagen del vehículo" style="max-width: 100%; height: auto;" />
-              <p id="cardTitle{{vehiculo.id}}" style="font-family: 'Poppins', sans-serif">Id: {{ vehiculo.id }}</p>
-              <p style="font-family: 'Poppins', sans-serif">Marca/Modelo: {{ vehiculo.marca }}/{{ vehiculo.modelo }}</p>
-              <h1 style="font-family: 'Poppins', sans-serif; color: #FF7A00">Estado: {{ vehiculo.estadoRenta }}</h1>
+              <p>Marca: {{ alquiler.vehiculo ? alquiler.vehiculo.marca : 'No disponible' }}</p>
+              <p>Modelo: {{ alquiler.vehiculo ? alquiler.vehiculo.modelo : 'No disponible' }}</p>
+              <p>Propietario: {{ alquiler.propietario ? alquiler.propietario.nombres : 'No disponible' }}</p>
+              <p>Telefono: {{ alquiler.propietario ? alquiler.propietario.telefono : 'No disponible' }}</p>
+              <p>Fecha de inicio: {{ alquiler.fecha_inicio }}</p>
+              <p>Fecha de fin: {{ alquiler.fecha_fin }}</p>
+
+              <!-- Agregar botón para ir a pagar si el estado del alquiler es 'Aceptado' -->
+              <div v-if="alquiler.estado === 'Aceptado'"><h3>Solicitud aceptada</h3></div>
+              <button v-if="alquiler.estado === 'Aceptado'" @click="irAPagar(alquiler)" class="custom-button">Ir a
+                Pagar
+              </button>
+              <div v-if="alquiler.estado === 'Pendiente'"><h3>Solicitud en espera</h3></div>
+              <div v-if="alquiler.estado === 'Rechazado'"><h3>Solicitud rechazada</h3></div>
+              <div v-if="alquiler.estado === 'Pagado'"><h3>Pagado</h3></div>
             </template>
           </Card>
         </div>
       </div>
-      <h2 v-else style="font-family: 'Poppins', sans-serif; color: black;" >No tiene vehículos alquilados actualmente.</h2>
     </div>
   </div>
-
 </template>
-
 
 <style>
 .custom-bg {
-  background-color: white;
+  background-color: #FFFFFF;
 }
 
 .custom-button {
-  background-color: white;
-  color: #14131B;
+  background-color: #FFFFFF;
+  color: #000000;
 }
 
 .custom-button:hover,
 .custom-button:focus {
   background-color: #FF7A00 !important;
-  color: white !important;
+  color: #FFFFFF !important;
 }
 
-.custom-button3 {
-  height: 30px;
-  margin: 20px;
-  background-color: black;
-  color: white;
-  font-family: 'Poppins', sans-serif;
-  border: none;
-  min-width: auto;
-}
-
-.custom-toolbar {
-  border-bottom: 2px solid #ddd;
-}
-
-.card-container {
-  display: flex;
-  flex-wrap: wrap;
-  gap: 10px;
+.custom-card {
+  border: 2px solid #92C9ED;
+  border-radius: 10px;
+  padding: 20px;
+  margin-bottom: 20px;
 }
 </style>
